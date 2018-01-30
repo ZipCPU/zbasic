51ffb14b0e2f94c84b3312b32983f2273521a4be
ZipCPU <dgisselq@ieee.org>
2018-01-29 21:49:12 -0500

    Added copyright header to hello world (no copyright asserted)

--- sw/board/hello.c
+++ sw/board/hello.c
@@ -1,3 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename:	helloworld.c
+//
+// Project:	ZBasic, a generic toplevel impl using the full ZipCPU
+//
+// Purpose:	The original Helllo World program.  If everything works, this
+//		will print Hello World to the UART, and then halt the CPU--if
+//	run with no O/S.
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Gisselquist Technology asserts no ownership rights over this particular
+// hello world program.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//
 #include <stdio.h>
 
 int main(int argc, char **argv) {
